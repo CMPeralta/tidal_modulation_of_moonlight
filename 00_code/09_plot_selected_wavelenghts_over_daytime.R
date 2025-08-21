a0af417b9ef94a556ede26751a64ca3219254980
Carolina Peralta <peralta@evolbio.mpg.de>
2025-08-21 12:21:56 +0200

    added new scripts to extract tidal data and visualize with Dinard light recordings

--- 00_code/09_plot_selected_wavelenghts_over_daytime.R
+++ 00_code/09_plot_selected_wavelenghts_over_daytime.R
@@ -1,8 +1,17 @@
 #!/usr/bin/env Rscript
 
 library(tidyverse)
+library(scales)
 
-#FIGURE S4 - selected daylight wavelenghts over time  
+#SIMILAR TO FIGURE S4 - selected daylight wavelenghts over time 
+
+#IMPORTANT NOTE: 
+## Figure S4 from Peralta et al. has tidal data extracted manually from maree.info. 
+## This script produces a similar plot to that of the manuscript BUT the tidal data is not the same 
+## Here, I am using data from GESLA, which can be retrieved with an R package (see get_tidal_data_GESLA.R) as opposed to the maree.info data, which needs to be manually extracted
+#QUALITY CONTROL
+## Predicted tides from GESLA occur ~5 minutes later on average (± 5 min SD) than maree.info and water level is ~0.09 m higher (± 0.13 m SD) on average
+
 
 dinard_form <- as_tibble(read.csv("01_data/dinard_light_data_formatted.csv", row.names = NULL))
 dinard_form <- dinard_form %>%
@@ -48,6 +57,35 @@ dinard_form_day_norm <- dinard_form_day_norm %>%
   select(day_daylight_new,time_UTC_3,time_UTC_2,everything())
 
 
+###### tidal data 
+
+tides <- as_tibble(read.csv("01_data/tidal_data_GESLA_STmalo.csv"))
+
+tides <- tides %>%
+  mutate(time_date_UTC   = with_tz(datetime, "UTC"), 
+        date_UTC = as.Date(time_date_UTC), 
+        time_UTC = format(time_date_UTC, "%H:%M"))  %>%
+  select(-X)
+
+#filter for daylight and days of dinard plot 
+tides_filtered0 <- tides %>% 
+  filter(time_date_UTC >= as.POSIXct("2013-10-18 06:00",tz = "UTC"),
+          time_date_UTC < as.POSIXct("2014-01-31 17:55",tz = "UTC"))
+tides_filtered <- tides_filtered0[tides_filtered0$time_UTC >= "06:00:13" & tides_filtered0$time_UTC <= "17:55:13",]
+
+#give days 
+tides_filtered_days <- tides_filtered %>%
+  arrange(date_UTC, time_date_UTC) %>%
+  mutate(
+    idx = dense_rank(date_UTC),          
+    day_daylight = idx + 1) %>%
+  select(day_daylight, everything(), -idx)
+  
+#levels <- paste(rev(unique(sort(tides_filtered_4daylight_s_f$time3))))
+levels <- paste(unique(sort(tides_filtered_days$time_UTC)))
+#make factor with the correct order
+tides_filtered_days$time4 <- factor(tides_filtered_days$time_UTC, levels = levels)
+
 ###PLOTTING 
 
 cols_plot_subs_bins <- c("nm_317","nm_337","nm_358","nm_378","nm_398","nm_401","nm_425","nm_448","nm_472","nm_495","nm_502","nm_526","nm_549","nm_572","nm_596","nm_603","nm_626","nm_649","nm_673","nm_696","nm_703","nm_726","nm_749","nm_772","nm_795","nm_802","nm_825","nm_848","nm_871","nm_894","nm_900","nm_914","nm_927","nm_940","nm_953") #pick five per 100nm class, interspersed equally
@@ -58,8 +96,8 @@ ggplot() +
       scale_fill_gradient2(low = "black", high = "darkorange2", mid = "white", midpoint = 0.5,na.value="black",name=bquote(atop("Day light",(mW/m^-2/nm^-1)))) +
       scale_y_discrete(expand = c(0.0, 0.0), breaks = c("06:00", "09:25", "12:25","15:25","17:55")) + 
       scale_x_continuous(expand = c(0.0, 0.0), breaks = c(2,17,31,47,61,76,91,105)) + 
-      #geom_point(data =tides_filtered_4daylight_s_f, size =1, aes(x = day_daylight, y = time4,color = water_level_m)) + 
-      #scale_color_gradient(low=muted("blue"), high=muted("darkred"),name = bquote(atop("Water level","(m)"))) +
+      geom_point(data =tides_filtered_days, size =1, aes(x = day_daylight, y = time4,color = water_level_m)) + 
+      scale_color_gradient(low=muted("blue"), high=muted("darkred"),name = bquote(atop("Water level","(m)"))) +
        theme(axis.text.x = element_blank(), 
         panel.grid = element_blank(),
        axis.text.y = element_blank(), 
---
+++ 00_code/11_get_tidal_data_GESLA.R
@@ -0,0 +1,102 @@
+#!/user/bin/env Rscript 
+
+library(tidyverse)
+library(dplyr)
+library(lubridate)
+library(geslaR) #needs to be installed via CRAN, e.g. (1st istall arrow via conda, then in R: install.packages("geslaR", dependencies = TRUE, repos = "https://cloud.r-project.org")
+library(oce)
+library(readxl)
+library(patchwork)
+
+
+#IMPORTANT NOTE: 
+## Figure S4 from Peralta et al. has tidal data extracted manually from maree.info. This script gets hourly observed sea levels from GESLA, timing of high and low tides is an approximation! 
+## To get high/low tides, the data was fitted using a harmonic model (using oce::tidem)
+## Predicted tides from GESLA occur ~5 minutes later on average (± 5 min SD) than maree.info and water level is ~0.09 m higher (± 0.13 m SD) on average
+
+# ------------------------------------
+# 1) get FRANCE observations (GESLA)
+
+raw <- query_gesla(country = "FRA", year = 2013:2014, as_data_frame = TRUE)
+
+#search geographical location  
+#unique(raw$site_name[grepl("malo", raw$site_name, ignore.case = TRUE)])
+
+sm_all <- raw %>%
+  filter(site_name %in% c("Saint_Malo")) %>%
+  transmute(site_name,
+            datetime = as_datetime(date_time, tz = "Europe/Paris"),
+            sea_level_m  = as.numeric(sea_level)) %>%
+  arrange(site_name, datetime) %>%
+  filter(!is.na(sea_level_m))
+
+res_info <- sm_all %>%
+  group_by(site_name) %>%
+  summarize(dt = median(diff(datetime), na.rm = TRUE), .groups = "drop") %>%
+  arrange(dt)
+
+series_to_use <- res_info$site_name[1]  #pick smallest median timestep
+message("Using GESLA series: ", series_to_use)
+
+sm <- sm_all %>%
+  filter(site_name == series_to_use) %>%
+  arrange(datetime)
+
+# ------------------------------------
+# 2) harmonic fit + prediction (grid each 5 min)
+
+start_local <- ymd_hms("2013-09-01 00:00:00", tz = "Europe/Paris")
+end_local   <- ymd_hms("2014-02-01 00:00:00", tz = "Europe/Paris")
+
+fit <- oce::tidem(t = sm$datetime, x = sm$sea_level_m, latitude = 48.65)
+grid <- seq(from = start_local, to = end_local, by = "5 min")
+pred <- oce::predict.tidem(fit, newdata = grid)
+
+pred_df <- tibble(
+  datetime = grid,
+  level_m = as.numeric(pred))
+
+# ------------------------------------
+# 3) candidate highs/lows by neighbor test
+cands <- pred_df %>%
+mutate(
+    prev_val = dplyr::lag(level_m),
+    next_val = dplyr::lead(level_m),
+    type = dplyr::case_when(
+      !is.na(prev_val) & !is.na(next_val) & level_m > prev_val & level_m > next_val ~ "HIGH",
+      !is.na(prev_val) & !is.na(next_val) & level_m < prev_val & level_m < next_val ~ "LOW",
+      TRUE ~ NA_character_)) %>%
+filter(!is.na(type)) %>%
+select(datetime, type, level_m) %>%
+arrange(datetime)
+
+#enforce alternation + ≥ 3 h separation
+min_sep_hours <- 3
+keep <- logical(nrow(cands))
+last_t <- as.POSIXct(NA, tz = "Europe/Paris")
+last_type <- NA_character_
+
+for (i in seq_len(nrow(cands))) {
+  if (is.na(last_t)) {
+    keep[i] <- TRUE
+    last_t <- cands$datetime[i]
+    last_type <- cands$type[i]} 
+    else {
+    dt_h <- as.numeric(difftime(cands$datetime[i], last_t, units = "hours"))
+    if (dt_h >= min_sep_hours && cands$type[i] != last_type) {
+      keep[i] <- TRUE
+      last_t <- cands$datetime[i]
+      last_type <- cands$type[i]}}}
+
+tides <- cands[keep, ]
+
+# 4) format and save
+tz_local <- "Europe/Paris"
+tides_out <- tides %>%
+  mutate(
+    datetime_local = with_tz(datetime, tz_local)) %>%
+  transmute(datetime, type, water_level_m = level_m)
+
+write.csv(tides_out, "01_data/tidal_data_GESLA_STmalo.csv")
+
+
